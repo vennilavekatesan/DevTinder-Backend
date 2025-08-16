1ca0a09cc54cae66bf3686b2fb587661053c3d36
vennila <vennilavenkatesan707@gmail.com>
2025-08-16 13:06:13 +0530

    Exploring Routes

--- src/app.js
+++ src/app.js
@@ -1,9 +1,31 @@
 const express= require('express');
 const app= express();
 
-app.use((req,res)=>{
-    res.send('Hello, World!');
+//to make a get request 
+
+app.get('/user', (req,res)=>{
+    res.send({name:'vennila',age:'23', city:'chennai'});
+})
+
+// app.get('/user/:id', (req, res) => {
+//     const userId = req.params.id;
+//     res.send({message: `User ID is ${userId}`});
+// });
+
+app.get('/user/:userid/:name/:password', (req, res) => {
+    console.log(req.params);
+    const { userid, name, password } = req.params;
+    res.send({message: `User ID: ${userid}, Name: ${name}, Password: ${password}`});
 });
+
+//to make a post request
+app.post('/user',(req,res)=>{
+    res.send({message:'User created successfully'});
+})
+
+// app.use((req,res)=>{
+//     res.send('Hello, World!');
+// });
 
 
 app.listen(3000,()=>{
