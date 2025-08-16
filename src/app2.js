5574b5c32bef0ec4aa46921e921e9f26cb584308
vennila <vennilavenkatesan707@gmail.com>
2025-08-16 21:20:06 +0530

    Basic auth check for the middleware

--- src/app2.js
+++ src/app2.js
@@ -1,29 +1,18 @@
 const express = require('express');
 const app = express();
 
-app.use('/', (req,res)=>{
-    res.send('Hello Route Handler!');    
+app.get('/admin/getUserData', (req,res)=>{
+    // need to check the authentication and authorization
+    // if the user is not authenticated or authorized, send an error response
+    // else send the user data
+    const token = 'xyzhhhljl';
+    const isAdminAuthorized = token === 'xyz'; // this is just a dummy check, in real application you would check the token against a database or some other service
+    if(!isAdminAuthorized) {
+        res.status(401).send('Unauthorized: Admin access required');
+    }
+    res.send('User Data');
 })
 
-// to make a get request
-// multiple route handlers
-app.get('/user', (req, res, next)=>{
-    console.log('1st Route Handler');
-    // res.send('1st Response');   commenting this because it will prevent the next handler from executing
-    next();
-}, 
-(req, res)=>{
-    console.log('2nd Route Handler');
-}, 
-(req, res)=>{
-    console.log('3rd Route Handler');
-    res.send('Final Response'); // This will be the final response sent to the client
-});
-
-
-// here the above final response won't be work because the response 'hello Route Handler!' is already sent, 
-// reason is that the route is '/' so it will check for the '/' route first and send the response, 
-// so the next handlers won't be executed
 app.listen(7777, ()=>{
     console.log('Server is running on port 7777');
 })
